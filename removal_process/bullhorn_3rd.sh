c16d4ed123a0c4939d218874c63d4476aedb20ad
Mario Lenz <m@riolenz.de>
2024-11-28 19:04:49 +0100

    Update links

--- removal_process/bullhorn_3rd.sh
+++ removal_process/bullhorn_3rd.sh
@@ -7,6 +7,6 @@ then
     exit 1
 fi
 
-echo "The \`$1\` collection [is considered unmaintained]($2) and will be removed from the Ansible Community Package (ACP) $3 if no one starts maintaining it again before ACP $3. See [the Ansible Community Package Collections Removal Process](https://docs.ansible.com/ansible/devel/community/collection_contributors/collection_package_removal.html#canceling-removal).
+echo "The \`$1\` collection [is considered unmaintained]($2) and will be removed from the Ansible Community Package (ACP) $3 if no one starts maintaining it again before ACP $3. See [the Ansible Community Package Collections Removal Process](https://docs.ansible.com/ansible/devel/community/collection_contributors/collection_package_removal.html#canceling-removal-of-an-unmaintained-collection).
 
 Please note that you can still manually install the collection with \`ansible-galaxy collection install $1\` even when it has been removed from the Ansible Community Package."
--- removal_process/repo_issue.sh
+++ removal_process/repo_issue.sh
@@ -8,6 +8,6 @@ then
 fi
 echo "Unmaintained collection: Removal from the Ansible Community Package
 
-It looks like this collection is effectively unmaintained. According to the current [Ansible Community Package Collections Removal Process](https://docs.ansible.com/ansible/devel/community/collection_contributors/collection_package_removal.html#unmaintained-collections), we will consider removing it in a future version of the Ansible Community Package. Please see [Unmaintained collection: $1]($2) for more information.
+It looks like this collection is effectively unmaintained. According to the current [Ansible Community Package Collections Removal Process](https://docs.ansible.com/ansible/devel/community/collection_contributors/collection_package_removal.html#identifying-and-removing-an-unmaintained-collection-that-has-not-been-deprecated-by-its-maintainers), we will consider removing it in a future version of the Ansible Community Package. Please see [Unmaintained collection: $1]($2) for more information.
 
 At least one month after this announcement appears here and on [Bullhorn](https://forum.ansible.com/c/news/bullhorn/17), the Ansible Community Steering Committee will vote on whether this collection is considered unmaintained and will be removed, or whether it will be kept. If it will be removed, this will happen earliest in Ansible $3. Please note that people can still manually install the collection with \`ansible-galaxy collection install $1\` even when it has been removed from the Ansible Community Package."
